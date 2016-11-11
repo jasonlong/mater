255a4733e4272adc59460770492c5b6a1b7eeb45
Jason Long <jlong@github.com>
2016-11-11 15:01:03 -0500

    5 second countdown working

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const mb = menubar({width: 220, height: 220})
 
 mb.on('ready', () => {
   console.log('app is ready')
-  // your app code here
 })
 
 mb.on('after-create-window', () => {
   mb.window.loadURL(`file://${__dirname}/index.html`)
 })
+
