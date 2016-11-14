8a0da41cbfdd7bf15c6a80e0a722ff42a9ad299c
Jason Long <jlong@github.com>
2016-11-14 15:48:19 -0600

    Super ugly ms timer working

---
+++ renderer.js
@@ -0,0 +1,25 @@
+const Timer = require('tiny-timer')
+
+const appContainer = document.querySelector('.js-app-container')
+const startBtn = document.querySelector('.js-start-btn')
+const stopBtn = document.querySelector('.js-stop-btn')
+const timeRemaining = document.querySelector('.js-time-remaining')
+
+let timer = new Timer()
+
+startBtn.addEventListener('click', () => {
+  timer.start(25 * 60 * 1000) // 25 seconds
+  appContainer.classList.add('is-running')
+})
+
+stopBtn.addEventListener('click', () => {
+  timer.stop()
+  appContainer.classList.remove('is-running')
+  timeRemaining.innerHTML = "1500000ms remaining"
+})
+
+timer.on('tick', (ms) => {
+  timeRemaining.innerHTML = ms + "ms remaining"
+})
+
+timer.on('done', () => console.log('done!'))
