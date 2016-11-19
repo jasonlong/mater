8ea307fcb57cbeb6e686ffa1b317a7ad8296cefe
Jason Long <jlong@github.com>
2016-11-19 10:54:52 -0500

    Add basic flexbox layout

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const electron = require('electron')
 const menubar = require('menubar')
-const mb = menubar({width: 220, height: 220})
+const mb = menubar({width: 220, height: 206})
 
 mb.on('ready', () => {
   console.log('app is ready')
