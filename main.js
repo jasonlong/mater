c44eeb6999961fe7180039e0add2c57973f67684
Jason Long <jlong@github.com>
2018-03-06 12:26:49 -0500

    Add right-click quit option

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-const electron = require('electron')
+const {electron, Menu} = require('electron')
 const menubar = require('menubar')
 const mb = menubar({
   width: 220,
   height: 206,
   preloadWindow: true,
-  // alwaysOnTop: true,
   icon: `${__dirname}/img/icon-0-Template.png`
 })
-
-require('electron-reload')(__dirname);
 
 // Make menubar accessible to the renderer
 global.sharedObject = {
@@ -21,6 +18,11 @@ mb.on('ready', () => {
 
 mb.on('after-create-window', () => {
   mb.window.loadURL(`file://${__dirname}/index.html`)
-  // mb.window.openDevTools()
-})
 
+  const contextMenu = Menu.buildFromTemplate ([
+    {label: 'Quit', click: () => {mb.app.quit ();}}
+  ])
+  mb.tray.on ('right-click', () => {
+    mb.tray.popUpContextMenu (contextMenu);
+  })
+})
